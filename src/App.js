647219e06c1f7594d9e0bcd32c0a60002d3069c6
ANUJ DEEP <36504458+anujdeep@users.noreply.github.com>
2022-03-31 01:32:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [userList,setUserList]=useState([]);
   let [userData,setUserData]=useState([]);
   const [page, setPage] = useState(1);
+  const [totalPage,setTotalPage]=useState(0);
   const [lastPageSize,setLastPageSize]=useState(10);
   useEffect(()=>{
     (async function() {
@@ -14,6 +15,7 @@ function App() {
       userList=userList.map((data)=>{
         return {...data,isChecked:false}
       });
+      setTotalPage(Math.ceil(userList.length/10));
       setUserData(userList);
       setUserList(userList.slice(0,10));
     })()
@@ -30,17 +32,13 @@ function App() {
   },[page,userData])
   
   function searchUser(searchedText){
-    
-    const startingDataIndex=(page-1)*10;
-    // const lastPageDataSize=userData.slice(startingDataIndex).length ;
-    const currentPageData=userData.slice(startingDataIndex,lastPageSize);
 
-    const searchedUserData=currentPageData.filter((userData)=>{
+    const searchedUserData=userData.filter((userData)=>{
       return userData.email.toLowerCase().includes(searchedText.toLowerCase())
       || userData.name.toLowerCase().includes(searchedText.toLowerCase())
       || userData.role.toLowerCase().includes(searchedText.toLowerCase())
     });
-    setUserList(searchedUserData);
+    setUserList(searchedUserData.slice(0,lastPageSize));
     
   }
 
@@ -51,16 +49,25 @@ function App() {
     const filteredUserData=userData.filter((userData)=>{
       return userData.id!==userId
     })
+    setTotalPage(Math.ceil(filteredUserData.length/10));
     setUserList(filteredData);
     setUserData(filteredUserData);
   }
 
   function selectOrDeselectHandler(value){
-    const userData=userList.map((userData)=>{
+    const updatedUserList=userList.map((userData)=>{
       return {...userData,isChecked:value}
     })
-    setUserList(userData);
-    setUserData(userData);
+    const updatedUserIds=updatedUserList.map((userData)=>userData.id);
+    const updatedUserData=userData.map((userData)=>{
+      if(updatedUserIds.includes(userData.id)){
+        return {...userData,isChecked:value};
+      }
+      else
+      return userData;
+    })
+    setUserList(updatedUserList);
+    setUserData(updatedUserData);
   }
 
   function singleSelectUserHandler(data){
@@ -92,19 +99,28 @@ function App() {
     const filteredUserData=userData.filter((userData)=>{
       return !userData.isChecked
     })
+    setTotalPage(Math.ceil(filteredUserData.length/10));
     setUserList(filteredData);
     setUserData(filteredUserData);
-    console.log("delete multiple users");
   }
 
   function changePageData(pageNumber){
-    setPage(pageNumber)  
+     
+    const filteredData=userList.map((userData)=>{
+      return {...userData,isChecked:false}
+    });
+    const filteredUserData=userData.filter((userData)=>{
+      return {...userData,isChecked:false}
+    })
+    setUserList(filteredData);
+    setUserData(filteredUserData);
+    setPage(pageNumber) 
   }
 
   return (
     <div className="App">
       <MainLayout>
-        <Table page={page} userList={userList} onInputChange={searchUser} onDeleteUser={deleteUserHandler}
+        <Table page={page} totalPage={totalPage} userList={userList} onInputChange={searchUser} onDeleteUser={deleteUserHandler}
         onSelectOrDeselectAll={selectOrDeselectHandler} onSingleSelectUser={singleSelectUserHandler}
         onUpdateUserData={updateUserDataHandler} onDeleteSelectedUser={deleteAllSelectedUser}
         onChangePage={changePageData}/>
